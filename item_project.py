4e04a90b2a4d063ae3902fd7802d6c691a415022
RedaMansy <mansyR@cardiff.ac.uk>
2018-10-22 15:41:32 +0100

    Item descriptions. Character descriptions are yet to be done.

---
+++ item_project.py
@@ -0,0 +1,163 @@
+item_keycard = {
+	"id": "keycard",
+
+	"name": "Keycard",
+
+	"description":
+	"""You need this to access the lab. That's if you want to do any work. 
+	You could just lose the game there's literally nothing riding on it."""
+
+} 
+
+item_github = {
+	"id" = "github",
+
+	"name": "GitHub",
+
+	"description":
+	"""Set up github to work from home on your laptop.
+	You can't access the code without having set this up."""
+}
+
+item_notes = {
+	"id" = "notes",
+
+	"name": "Notes",
+
+	"description":
+	"""===DESCRIPTION==="""
+}
+
+item_voucher = {
+	"id" = "voucher",
+
+	"name": "Voucher",
+
+	"description":
+	"""Free laptop voucher. Yep, free laptop. Cheaper than the cafeteria food."""
+}
+
+item_food = {
+	"id": "food",
+
+	"name": "Food",
+
+	"description":
+	"""You need this for energy. But you knew that already... I hope.""",
+
+	"Price": = 0.0
+}
+
+item_water = {
+	"id": "water",
+
+	"name": "Water",
+
+	"description":
+	"""Water is essential for survival. Without water you die, incase you didn't know.
+	That's real life though, don't know about this game. You could try not drinking water
+	just to see what happens. """,
+
+	"Price": = 0.0
+}
+
+item_bluebear = {
+	"id": "bluebear",
+
+	"name": "Bluebear",
+
+	"description":
+	"""Redbull knock off. Doubles your energy, then halves it the next day.""",
+
+	"Price": 0.0
+}
+
+item_laptop = {
+	"id": "laptop",
+
+	"name": "Laptop Computer",
+
+	"description":
+	"""It's a free laptop. I think it's good enough for its low cost of nothing. 
+	You need it for coding at home. Gotta have github though.""",
+
+	"Price": 0.0
+}
+
+item_phone{
+	"id": "phone",
+
+	"name": "Mobile Phone",
+
+	"description":
+	"""This is how you communicate with others. And watch porn."""
+}
+
+item_NICoffee{
+	"id": "NICoffee"
+
+	"name": "Northern Irish Coffee",
+
+	"description":
+	"""It seem like a coffee, but it is a beer. -Elwin"""	
+}
+
+item_turing{
+	"id": "turing",
+
+	"name": "Turing",
+
+	"description":
+	"""===DESCRIPTION==="""
+}
+
+item_profoak{
+	"id": "profoak",
+
+	"name": "Profoak",
+
+	"description":
+	"""===DESCRIPTION==="""
+}
+
+"""
+================================================================================
+========================Character list but not item list========================
+================================================================================
+"""
+item_thebae{
+	"id": "thebae",
+
+	"name": "Bae",
+
+	"description":
+	"""===DESCRIPTION==="""
+}
+
+item_zucc{
+	"id": "zucc",
+
+	"name": "Zucc",
+
+	"description":
+	"""===DESCRIPTION==="""
+}
+
+item_hideokojima{
+	"id": "hideokojima",
+
+	"name": "Hideo Kojima",
+
+	"description":
+	"""===DESCRIPTION==="""	
+}
+
+item_pythonguy{
+	"id": "pythonguy",
+
+	"name": "The Python Guy",
+
+	"description":
+	"""===DESCRIPTION==="""
+}
+

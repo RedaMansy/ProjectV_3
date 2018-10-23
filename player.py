493c31452f0da09f4dd7da8a9d2918a7b063ec5d
Elwin221039 <heiheichoy@yahoo.com.hk>
2018-10-23 11:33:56 +0100

    Added player code

---
+++ player.py
@@ -0,0 +1,12 @@
+from items import *
+from map import rooms
+
+inventory = []
+#For example: item_id
+
+# Start game at the reception
+current_room = rooms["Reception"]
+
+energy_bar = 100
+
+project_process = 0
